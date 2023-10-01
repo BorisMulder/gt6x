b810fe65dca2a13f36c2208af43e37b60f8f1764
Boris Mulder <boris-120@hotmail.com>
2023-10-01 18:55:57 +0200

    Added Ion Implantation

--- src/main/java/org/altadoon/gt6x/features/ree/REEProcessing.java
+++ src/main/java/org/altadoon/gt6x/features/ree/REEProcessing.java
@@ -7,9 +7,9 @@ import org.altadoon.gt6x.common.Config;
 import org.altadoon.gt6x.common.MTx;
 import org.altadoon.gt6x.features.GT6XFeature;
 
-import static gregapi.data.CS.NF;
-import static gregapi.data.CS.U;
+import static gregapi.data.CS.*;
 import static gregapi.data.OP.dust;
+import static gregapi.data.OP.dustSmall;
 
 public class REEProcessing  extends GT6XFeature {
     public static final String FEATURE_NAME = "REEProcessing";
@@ -27,6 +27,7 @@ public class REEProcessing  extends GT6XFeature {
     public void postInit() {}
 
     private void addRecipes() {
+        RM.Roasting.addRecipe1(true, 16, 5*128, dust.mat(MT.Y, 1), MT.O.gas(3*U2, true), NF, dustSmall.mat(MTx.Y2O3, 10));
         RM.Roasting.addRecipe1(true, 16, 3*128, dust.mat(MT.Ce, 1), MT.O.gas(2*U, true), NF, dust.mat(MTx.CeO2, 3));
         RM.Bath.addRecipe1(true, 0, 3*128, dust.mat(MTx.CeO2, 3), FL.array(MT.HNO3.liquid(30*U, true)), FL.array(MTx.NitratoCericAcid.liquid(27*U, false), MT.H2O.liquid(6*U, false)));
     }
