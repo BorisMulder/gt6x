c360fd7ea1c3e45fab15c2ae249ca73085ab2be0
Boris Mulder <boris-120@hotmail.com>
2024-03-16 19:08:46 +0100

    Added Se/Te processing from Cu anode sludge

--- src/main/java/org/altadoon/gt6x/common/RMx.java
+++ src/main/java/org/altadoon/gt6x/common/RMx.java
@@ -1,7 +1,6 @@
 package org.altadoon.gt6x.common;
 
 import gregapi.data.FL;
-import gregapi.recipes.Recipe;
 import gregapi.recipes.Recipe.RecipeMap;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -9,23 +8,30 @@ import static gregapi.data.CS.RES_PATH_GUI;
 
 /** Common recipe maps */
 public class RMx {
+    public static RecipeMap BasicOxygen = null;
+    public static RecipeMap BlastFurnace = null;
+    public static RecipeMap CowperStove = null;
+    public static RecipeMap DirectReduction = null;
+    public static RecipeMap IonBombardment = null;
     public static RecipeMap Ionizer = null;
+    public static RecipeMap Photolithography = null;
     public static RecipeMap Sintering = null;
+    public static RecipeMap Soldering = null;
     public static RecipeMap Thermolysis = null;
-
-    public static Recipe.RecipeMap Photolithography = null;
-    public static Recipe.RecipeMap IonBombardment = null;
-    public static Recipe.RecipeMap Soldering = null;
-
+    
     public static final FluidStack[] CuttingFluids = { FL.Water.make(1000), FL.SpDew.make(1000), FL.DistW.make(1000), FL.Lubricant.make(1000), FL.LubRoCant.make(1000) };
     public static final long[] CuttingMultiplier = {4, 4, 3, 1, 1};
 
     public static void init() {
-        Ionizer = new RecipeMap(null, "gt6x.recipe.ionizer"  , "Ionization", null, 0, 1, RES_PATH_GUI+"machines/Ionizer"  , 0, 0, 0, 1, 1, 1, 1, 1, "", 1, "", true, true, true, true, false, true, true);
-        Sintering = new RecipeMap(null, "gt6x.recipe.sintering", "Sintering" , null, 0, 1, RES_PATH_GUI+"machines/Sintering", 6, 1, 1, 0, 0, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
-        Thermolysis = new RecipeMap(null, "gt6x.recipe.thermolysis", "Thermal Decomposition", null, 0, 1, RES_PATH_GUI+"machines/thermolysis", 2, 6, 0, 2, 6, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
-        Photolithography = new Recipe.RecipeMap(null, "gt6x.recipe.photolithography", "Photolithography", null, 0, 1, RES_PATH_GUI+"machines/Photolithography", 3, 1, 2, 3, 3, 1, 2, 1, "", 1, "", true, true, true, true, false, true, true);
-        IonBombardment = new Recipe.RecipeMap(null, "gt6x.recipe.ionbombardment", "Ion Acceleration", null, 0, 1, RES_PATH_GUI+"machines/IonBombardment", 2, 1, 1, 1, 2, 0, 2, 1, "", 1, "", true, true, true, true, false, true, true);
-        Soldering = new Recipe.RecipeMap(null, "gt6x.recipe.soldering", "Soldering Machine", null, 0, 1, RES_PATH_GUI+"machines/Soldering", 9, 1, 1, 1, 0, 1, 2, 1, "", 1, "", true, true, true, true, false, true, true);
+        Sintering        = new RecipeMap(null, "gt6x.recipe.sintering"       , "Sintering"               , null, 0, 1, RES_PATH_GUI+"machines/Sintering"       , 6, 1, 1, 0, 0, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
+        Thermolysis      = new RecipeMap(null, "gt6x.recipe.thermolysis"     , "Thermal Decomposition"   , null, 0, 1, RES_PATH_GUI+"machines/thermolysis"     , 2, 6, 0, 2, 6, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
+        Ionizer          = new RecipeMap(null, "gt6x.recipe.ionizer"         , "Ionization"              , null, 0, 1, RES_PATH_GUI+"machines/Ionizer"         , 0, 0, 0, 1, 1, 1, 1, 1, "", 1, "", true, true, true, true, false, true, true);
+        Photolithography = new RecipeMap(null, "gt6x.recipe.photolithography", "Photolithography"        , null, 0, 1, RES_PATH_GUI+"machines/Photolithography", 3, 1, 2, 3, 3, 1, 2, 1, "", 1, "", true, true, true, true, false, true, true);
+        IonBombardment   = new RecipeMap(null, "gt6x.recipe.ionbombardment"  , "Ion Acceleration"        , null, 0, 1, RES_PATH_GUI+"machines/IonBombardment"  , 2, 1, 1, 1, 2, 0, 2, 1, "", 1, "", true, true, true, true, false, true, true);
+        Soldering        = new RecipeMap(null, "gt6x.recipe.soldering"       , "Soldering Machine"       , null, 0, 1, RES_PATH_GUI+"machines/Soldering"       , 9, 1, 1, 1, 0, 1, 2, 1, "", 1, "", true, true, true, true, false, true, true);
+        BlastFurnace     = new RecipeMap(null, "gt6x.recipe.blastfurnace"    , "Blast Furnace"           , null, 0, 1, RES_PATH_GUI+"machines/BlastFurnace"    , 6, 3, 1, 3, 3, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
+        CowperStove      = new RecipeMap(null, "gt6x.recipe.cowperstove"     , "Hot Blast Preheating"    , null, 0, 1, RES_PATH_GUI+"machines/Default"         , 0, 0, 0, 1, 1, 1, 1, 1, "", 1, "", true, true, true, true, false, true, true);
+        BasicOxygen      = new RecipeMap(null, "gt6x.recipe.bop"             , "Basic Oxygen Steelmaking", null, 0, 1, RES_PATH_GUI+"machines/BlastFurnace"    , 6, 3, 1, 3, 3, 1, 2, 1, "", 1, "", true, true, true, true, false, true, true);
+        DirectReduction  = new RecipeMap(null, "gt6x.recipe.directreduction" , "Direct Reduction"        , null, 0, 1, RES_PATH_GUI+"machines/DirectReduction" , 6, 3, 1, 3, 3, 1, 1, 1, "", 1, "", true, true, true, true, false, true, true);
     }
 }
