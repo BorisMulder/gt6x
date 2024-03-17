85490e04da8d69ef68d2919aeff44fb720398426
Boris Mulder <boris-120@hotmail.com>
2024-03-17 17:21:01 +0100

    Thermolysis oven: Add extra input slots, fixup wobbly animation

--- src/main/java/org/altadoon/gt6x/common/RMx.java
+++ src/main/java/org/altadoon/gt6x/common/RMx.java
@@ -18,13 +18,13 @@ public class RMx {
     public static RecipeMap Sintering = null;
     public static RecipeMap Soldering = null;
     public static RecipeMap Thermolysis = null;
-    
+
     public static final FluidStack[] CuttingFluids = { FL.Water.make(1000), FL.SpDew.make(1000), FL.DistW.make(1000), FL.Lubricant.make(1000), FL.LubRoCant.make(1000) };
     public static final long[] CuttingMultiplier = {4, 4, 3, 1, 1};
 
     public static void init() {
         Sintering        = new RecipeMap(null, "gt6x.recipe.sintering"       , "Sintering"               , null, 0, 1, RES_PATH_GUI+"machines/Sintering"       , 6, 1, 1, 0, 0, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
-        Thermolysis      = new RecipeMap(null, "gt6x.recipe.thermolysis"     , "Thermal Decomposition"   , null, 0, 1, RES_PATH_GUI+"machines/thermolysis"     , 2, 6, 0, 2, 6, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
+        Thermolysis      = new RecipeMap(null, "gt6x.recipe.thermolysis"     , "Thermal Decomposition"   , null, 0, 1, RES_PATH_GUI+"machines/thermolysis"     , 3, 6, 0, 3, 6, 0, 1, 1, "", 1, "", true, true, true, true, false, true, true);
         Ionizer          = new RecipeMap(null, "gt6x.recipe.ionizer"         , "Ionization"              , null, 0, 1, RES_PATH_GUI+"machines/Ionizer"         , 0, 0, 0, 1, 1, 1, 1, 1, "", 1, "", true, true, true, true, false, true, true);
         Photolithography = new RecipeMap(null, "gt6x.recipe.photolithography", "Photolithography"        , null, 0, 1, RES_PATH_GUI+"machines/Photolithography", 3, 1, 2, 3, 3, 1, 2, 1, "", 1, "", true, true, true, true, false, true, true);
         IonBombardment   = new RecipeMap(null, "gt6x.recipe.ionbombardment"  , "Ion Acceleration"        , null, 0, 1, RES_PATH_GUI+"machines/IonBombardment"  , 2, 1, 1, 1, 2, 0, 2, 1, "", 1, "", true, true, true, true, false, true, true);
